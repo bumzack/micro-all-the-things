7c094b5c36edadbd9d36565b4e1b27b811f31678
bumzack <bumzacktrara@gmail.com>
2023-02-21 10:59:24 +0100

    micro fe

---
+++ frontend/pov-micro-fe/productsapp/serve_product_app.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const serve = require('koa-static')
+const path = require('path')
+
+const app = new Koa()
+app.use(serve(path.join(__dirname, './')))
+
+console.log("path " +  path.join(__dirname, './'))
+app.listen(3011)
