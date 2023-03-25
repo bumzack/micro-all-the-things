1995f050ed86bd2b0ffd225d63b80531e5b274e8
bumzack <bumzacktrara@gmail.com>
2023-03-25 20:40:31 +0100

    fmt (#3)

--- code/tooling/gateway/src/main/java/at/bumzack/gateway/GatewayApplication.java
+++ code/tooling/gateway/src/main/java/at/bumzack/gateway/GatewayApplication.java
@@ -9,19 +9,19 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator myRoutes(final RouteLocatorBuilder builder) {
+    @Bean
+    public RouteLocator myRoutes(final RouteLocatorBuilder builder) {
 
-		// TODO: https://github.com/spring-cloud/spring-cloud-gateway/issues/276
-		return builder.routes()
-				.route(p -> p
-						.path("/api/person")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri( "http://localhost:3040/api/person"))
-				.build();
-	}
+        // TODO: https://github.com/spring-cloud/spring-cloud-gateway/issues/276
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/person")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:3040/api/person"))
+                .build();
+    }
 }
