571ca6aa04829da0cbf1c30b4b3a295a449a2a36
bumzack <bumzacktrara@gmail.com>
2023-03-26 14:33:10 +0200

    solr schema updates for imdb data

--- scripts/start_solr.sh
+++ scripts/start_solr.sh
@@ -2,7 +2,7 @@
 
 DIR=$PWD
 echo "starting SolR on port 8984"
-cd /Users/gsc/stoff/mylogalyzer/solr-10.0.0-SNAPSHOT/bin
+cd /Users/gsc/stoff/micro-all-the-things/solr-10.0.0-SNAPSHOT/bin
 ./solr start -p 8984
 
 cd $DIR
