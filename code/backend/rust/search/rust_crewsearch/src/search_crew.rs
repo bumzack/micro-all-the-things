98e8fab51e3e5a0591b10bc5e1e20eb6471d6bcd
bumzacktrara <bumzacktrara@gmail.com>
2023-04-03 22:47:02 +0200

    fmt

--- code/backend/rust/search/rust_crewsearch/src/search_crew.rs
+++ code/backend/rust/search/rust_crewsearch/src/search_crew.rs
@@ -8,22 +8,23 @@ pub mod filters_search_crew {
 
     use crate::CLIENT;
 
-    pub fn search_crew_route() -> impl Filter<Extract=(impl warp::Reply, ), Error=warp::Rejection> + Clone
-    {
+    pub fn search_crew_route(
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
         let server = warp::path!("api" / "crew" / String);
-        server
-            .and(warp::get())
-            .and_then(|tconst: String| {
-                println!("/api/crew/:tconst matched");
-                filter_entity("crew".to_string(), "tconst".to_string(), tconst)
-            })
+        server.and(warp::get()).and_then(|tconst: String| {
+            println!("/api/crew/:tconst matched");
+            filter_entity("crew".to_string(), "tconst".to_string(), tconst)
+        })
     }
 
-    fn filter_entity(entity: String, attribute: String, value: String) -> impl Future<Output=Result<impl Reply + Sized, Infallible>> {
+    fn filter_entity(
+        entity: String,
+        attribute: String,
+        value: String,
+    ) -> impl Future<Output = Result<impl Reply + Sized, Infallible>> {
         println!("filter_entity  {attribute} =  {value}");
         let f = format!("\"{}\"  = \"{}\"", attribute, value);
         let filter: Vec<String> = vec![f];
         meili_filter_crew(entity, filter, &CLIENT)
     }
 }
-
