ff9334c31c97a8e963abd499c1b23d2f83178d4f
bumzacktrara <bumzacktrara@gmail.com>
2023-04-16 01:14:20 +0200

    docu

--- code/backend/rust/ubuntu/create_search_docs.sh
+++ code/backend/rust/ubuntu/create_search_docs.sh
@@ -5,7 +5,7 @@ echo "started  building of searchindex at  ${START}"
 
 
 echo "start, pagesize, tasks"
-curl -vvv http://localhost:18300/api/v3/solr/searchindex/build/0/50000/8
+curl -vvv http://localhost:18300/api/v3/solr/searchindex/build/9728000/50000/8
 
 END=$(date +"%d-%m-%Y %T")
 
