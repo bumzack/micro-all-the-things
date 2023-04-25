2f43434b5da818c742ceb54c0856cb40511cb391
bumzack <bumzacktrara@gmail.com>
2023-04-25 22:23:53 +0200

    getting there, we have headers!! (#65)

--- code/frontend/article_search_results/feapp/dist/index.6b815632.js
+++ code/frontend/article_search_results/feapp/dist/index.6b815632.js
@@ -595,7 +595,9 @@ window.$ = window.jQuery = (0, _jqueryDefault.default);
             event.preventDefault();
             const txt = (0, _jqueryDefault.default)("#searchMovie").val();
             console.log(`return pressed     ${txt}  `);
-            const url = "http://proxy.proxythingi.at/rust/solr/search";
+            const url_prod = "http://proxy.proxythingi.at/rust/meili/search";
+            const url_local = "http://localhost:18600/api/v1/solr/article";
+            const url = url_prod;
             const customer = {
                 customer_id: 1,
                 jwt: "eyJhbGciOiJIUzM4NCJ9.eyJjdXN0b21lcl9pZCI6IjEifQ.ygrMNXNsg00VwM6u0mk_WlUZvYKlVYDCgOi7trRnw3MrcEnwu-zIp-JbNCYqNlp9"
@@ -603,7 +605,7 @@ window.$ = window.jQuery = (0, _jqueryDefault.default);
             const req = {
                 q: txt,
                 offset: 0,
-                limit: 5,
+                limit: 10,
                 customer: customer
             };
             console.log(`sending request  to url ${url}. req  ${JSON.stringify(req, null, 4)} `);
@@ -645,23 +647,91 @@ window.$ = window.jQuery = (0, _jqueryDefault.default);
                         cache: "no-cache",
                         credentials: "same-origin",
                         headers: {
-                            "Content-Type": "application/json"
+                            "Content-Type": "application/json",
+                            //   "Access-Control-Expose-Headers": "x-duration,x-provided-by,x-initiated-by,x-processed-by"
+                            "Access-Control-Expose-Headers": "*"
                         },
-                        redirect: "follow",
-                        referrerPolicy: "no-referrer",
+                        // redirect: "follow", // manual, *follow, error
+                        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
                         body: JSON.stringify(data)
                     });
-                    return response.json(); // parses JSON response into native JavaScript objects
+                    for (const [h1, h2] of response.headers)console.log(h1 + ": " + h2);
+                    return [
+                        (yield response.json()),
+                        response.headers
+                    ]; // parses JSON response into native JavaScript objects
                 });
             }
-            postData(url, req).then((data)=>{
+            postData(url, req).then(([data, headers])=>{
                 console.log(data); // JSON data parsed by `data.json()` call
+                console.log(headers); // JSON data parsed by `data.json()` call
+                for (const header of headers.entries())console.log(header[0] + ": " + header[1]);
+                const dur = headers.get("x-duration");
+                const provided_by = headers.get("x-provided-by");
+                const initiated_by = headers.get("x-initiated-by");
+                const processed_by = headers.get("x-processed-by");
+                (0, _jqueryDefault.default)("#dur").text(dur);
+                (0, _jqueryDefault.default)("#provided_by").text(provided_by);
+                (0, _jqueryDefault.default)("#initiated_by").text(initiated_by);
+                (0, _jqueryDefault.default)("#processed_by").text(processed_by);
+                console.log(`proxythingi backend  ${provided_by},  duration  ${dur}    initiated_by  ${initiated_by}   processed_by  ${processed_by} `);
                 const movies = data;
                 console.log(`movies.len ${movies.articles.length} `);
+                (0, _jqueryDefault.default)("#search_results").empty();
+                (0, _jqueryDefault.default)("#search_results").text(`${movies.articles.length} movies found`);
+                movies.articles.forEach((a)=>{
+                    const elem = article_template(a);
+                    (0, _jqueryDefault.default)("#search_results").append(elem);
+                });
             });
         }
     });
 });
+const article_template = (article)=>{
+    let titles = "";
+    if (article.article.titles != undefined) titles = "Titles: " + article.article.titles.join(" / ");
+    let primary_title = "";
+    if (article.article.primary_title != undefined) primary_title = article.article.primary_title;
+    let original_title = "";
+    if (article.article.original_title != undefined) original_title = article.article.original_title;
+    let characters = "";
+    if (article.article.characters != undefined) characters = "Characters: " + article.article.characters.join(" / ");
+    let act = "";
+    if (article.article.actors != undefined) act = "Actors: " + article.article.actors.join(" / ");
+    let writers = "";
+    if (article.article.writers != undefined) writers = "Writers: " + article.article.writers.join(" / ");
+    let directors = "";
+    if (article.article.directors != undefined) directors = "Directors: " + article.article.directors.join(" / ");
+    let price;
+    if (article.customer_price !== undefined) price = `SRP € <sr>${article.price.toFixed(2)}</sr>, your price: € ${article.customer_price.toFixed(2)}`;
+    else price = `SRP € ${article.price} `;
+    return `   
+        <div class="col">
+            <div class="card h-100">
+                 <div class="card-body">
+                    <h5 class="card-title">original title${original_title}</h5>
+                    <h5 class="card-title">primary title${primary_title}</h5>
+                    <p class="card-body">${titles}</p>
+                    <p class="card-body">${act}</p>
+                    <p class="card-body">${characters}</p>
+                    <p class="card-body">${directors}</p>
+                    <p class="card-body">${writers}</p>
+                    <p class="card-body">ttconst ${article.article.tconst}</p>
+                 </div>
+                 <div class="card-footer">
+                    <div class="row row-cols-2 row-cols-md-2 g-6">
+                       <div class="col">
+                            ${price}
+                        </div>
+                    <div class="col">
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+    `;
+};
 
 },{"jquery":"hgMhh","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"hgMhh":[function(require,module,exports) {
 /*!
